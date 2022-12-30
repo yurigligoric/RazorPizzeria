37efaa16f1421b0ffc4ac1b67f2d0b15872afd1b
yurigligoric <yurigligoric@gmail.com>
2022-12-30 17:02:15 +0800

    first commit app alrady done

---
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Checkout
+{
+    [BindProperties(SupportsGet = true)]
+    public class CheckoutModel : PageModel
+    {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if(string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
+        }
+    }
+}
