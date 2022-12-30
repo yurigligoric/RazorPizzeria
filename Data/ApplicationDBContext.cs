8a35e471fc3e4950a435f69f3ac16dedb125c5cd
yurigligoric <yurigligoric@gmail.com>
2022-12-30 21:03:22 +0800

    added sql server

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/PizzaOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPizzeria.Models
+{
+    public class PizzaOrder
+    {
+        public int ID { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; }
+
+
+    }
+}
